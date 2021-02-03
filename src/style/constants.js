0947be64f9a19e1c53d7641eb631d1f205f67aa9
Jake Dluhy <jake.dluhy@producepay.com>
2021-02-02 21:54:57 -0800

    Restructure main app and add colors

---
+++ src/style/constants.js
@@ -0,0 +1,7 @@
+export const SIDEBAR_BACKGROUND = '#F1F5F8';
+export const BORDER = '#D1D9E3';
+export const FONT_COLOR = '#212121';
+export const PP_GREEN = '#33CE9B';
+export const ARROW_RED = '#EB5757';
+export const ARROW_GREEN = '#219653';
+export const NEUTRAL_GRAY = '#4F4F4F';
