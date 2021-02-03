c8d96f63039bcddaef1295401a7425e4f48404ed
Jake Dluhy <jake.dluhy@producepay.com>
2021-02-02 22:12:12 -0800

    Add icon examples

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+import Heart from 'react-feather/dist/icons/heart';
+import ArrowUp from 'react-feather/dist/icons/arrow-up';
+import ArrowDown from 'react-feather/dist/icons/arrow-down';
+import Minus from 'react-feather/dist/icons/minus';
+
+import {
+  PP_GREEN,
+  ARROW_RED,
+  ARROW_GREEN,
+  NEUTRAL_GRAY
+} from './style/constants';
+
 function App() {
   return (
     <div
@@ -12,6 +24,12 @@ function App() {
       }}
     >
       Main Screen
+
+      <Heart style={{ fill: PP_GREEN, stroke: PP_GREEN }} />
+      <Heart style={{ stroke: NEUTRAL_GRAY }} />
+      <ArrowUp style={{ stroke: ARROW_GREEN }} />
+      <ArrowDown style={{ stroke: ARROW_RED }} />
+      <Minus style={{ stroke: NEUTRAL_GRAY }} />
     </div>
   );
 }
